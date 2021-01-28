ca8a2fe02925fda5f90f5dcf3959bcbd45dc0b28
AHMET AKTAS <aktasahm16@itu.edu.tr>
2021-01-28 20:01:35 +0300

    Add project files.

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor
+
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArray = items; //geçiciyi tamamen önceki değerleri korumak için tutuyorum.
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++) //emaneten verdiğim verileri geri versene.tempArray'in
+                                                       //bütün elemanlarını items'a geri atayacağım.
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+            
+
+
+
+
+
+
+
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] isimler = new string[] {"Engin", "Murat", "Kerem", "Halil" }; //sol taraf stack sağ taraf heap adresi aldı heap adres.
+
+
+
+            List<string> isimler2 = new List<string>();
+        
+        }
+
+    }
+}
